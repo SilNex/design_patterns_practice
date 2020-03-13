a17b1abdcf8e34a60697dbbaa8f80b626ce46eb4
silnex <silnex@silnex.kr>
2020-03-14 01:19:37 +0900

    Add comment

--- head-first/Command/clinet.php
+++ head-first/Command/clinet.php
@@ -7,10 +7,12 @@ require('./Command.php');
 require('./Reciver.php');
 require('./RemoteControl.php');
 
-
+// Invoker
 $remoteControl = new RemoteControl();
+// Receiver
 $light = new Light();
 
+// Command or ConcreteCommand
 $lightOn = new LightOnCommand($light);
 $lightOff = new LightOffCommand($light);
 
